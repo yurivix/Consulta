ff5af2488308fe9480f4bd80d923ea6379a4f85d
yurivix <yuri.vix@gmail.com>
2025-04-10 10:18:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, request, render_template
 from zeep import Client
 from zeep.transports import Transport
-from requests.auth import HTTPBasicAuth
-import requests
 import os
 
 app = Flask(__name__)
 
-# Pegando usuário, senha e idConsultante das variáveis de ambiente
-PJE_USER = os.getenv('PJE_USER')
-PJE_PASS = os.getenv('PJE_PASS')
-ID_CONSULTANTE = os.getenv('ID_CONSULTANTE')  # <-- adicione isso no Render também
+# Variáveis de ambiente
+ID_CONSULTANTE = os.getenv('ID_CONSULTANTE')
+SENHA_CONSULTANTE = os.getenv('SENHA_CONSULTANTE')
 
-# WSDL do serviço de intercomunicação
+# WSDL do serviço
 WSDL_URL = 'https://pje.tjes.jus.br/pje/intercomunicacao?wsdl'
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,17 +18,12 @@ def index():
         numero_processo = request.form['numero_processo']
 
         try:
-            # Sessão autenticada
-            session = requests.Session()
-            session.auth = HTTPBasicAuth(PJE_USER, PJE_PASS)
-            transport = Transport(session=session)
+            client = Client(wsdl=WSDL_URL)
 
-            # Cliente SOAP com autenticação
-            client = Client(wsdl=WSDL_URL, transport=transport)
-
-            # Consulta ao processo com os dois parâmetros
+            # Chamada com todos os parâmetros obrigatórios
             response = client.service.consultarProcesso(
                 idConsultante=ID_CONSULTANTE,
+                senhaConsultante=SENHA_CONSULTANTE,
                 numeroProcesso=numero_processo
             )
 
